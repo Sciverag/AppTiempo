64412a8c98621b0f9a03abbdd8e30f74f9e4539d
sciverag <samuelciga2004@gmail.com>
2024-04-23 19:31:30 +0200

    Primera version

--- app/src/main/java/com/example/myweatherbase/activities/MainActivity.java
+++ app/src/main/java/com/example/myweatherbase/activities/MainActivity.java
@@ -1,60 +1,70 @@
 package com.example.myweatherbase.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Spinner;
 
-import com.example.myweatherbase.API.Connector;
 import com.example.myweatherbase.R;
+import com.example.myweatherbase.activities.model.Ciudad;
+import com.example.myweatherbase.activities.model.CiudadRepository;
 import com.example.myweatherbase.activities.model.Root;
 import com.example.myweatherbase.base.BaseActivity;
-import com.example.myweatherbase.base.CallInterface;
 import com.example.myweatherbase.base.ImageDownloader;
-import com.example.myweatherbase.base.Parameters;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+public class MainActivity extends BaseActivity implements AdapterView.OnItemSelectedListener {
 
-public class MainActivity extends BaseActivity implements CallInterface {
+    private Spinner spinnerCiudades;
+    private ImageView ivCiudad;
+    private Button bttnPrevision;
+    private CiudadRepository ciudadRepository;
+    private Ciudad selectedCiudad;
 
-    private TextView txtView ;
-    private TextView textViewDay;
-    private TextView textViewDayOfWeek;
-    private ImageView imageView;
     private Root root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtView = findViewById(R.id.txtView);
-        textViewDay = findViewById(R.id.textViewDay);
-        textViewDayOfWeek = findViewById(R.id.textViewDayOfWeek);
-        txtView = findViewById(R.id.txtView);
-        imageView = findViewById(R.id.imageView);
+        spinnerCiudades = findViewById(R.id.spinnerCiudades);
+        ivCiudad = findViewById(R.id.ivIconoCiudad);
+        bttnPrevision = findViewById(R.id.bttnPrevison);
 
-        // Mostramos la barra de progreso y ejecutamos la llamada a la API
-        showProgress();
-        executeCall(this);
+        ImageDownloader.downloadImage("https://cdn.pixabay.com/photo/2015/11/18/16/03/valencia-1049389_1280.jpg",ivCiudad);
+
+        ciudadRepository = new CiudadRepository();
+        selectedCiudad = ciudadRepository.get(0);
+        ArrayAdapter<Ciudad> ciudadArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,ciudadRepository.getCiudades());
+        spinnerCiudades.setAdapter(ciudadArrayAdapter);
+
+        spinnerCiudades.setOnItemSelectedListener(this);
     }
 
-    // Realizamos la llamada y recogemos los datos en un objeto Root
     @Override
-    public void doInBackground() {
-        root = Connector.getConector().get(Root.class,"&lat=39.5862518&lon=-0.5411163");
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Ciudad ciudad = (Ciudad) adapterView.getItemAtPosition(i);
+        selectedCiudad = ciudad;
+        ImageDownloader.downloadImage(ciudad.getImageURL(),ivCiudad);
     }
 
-    // Una vez ya se ha realizado la llamada, ocultamos la barra de progreso y presentamos los datos
     @Override
-    public void doInUI() {
-        hideProgress();
-        txtView.setText(root.list.get(0).weather.get(0).description);
-        ImageDownloader.downloadImage(Parameters.ICON_URL_PRE + root.list.get(0).weather.get(0).icon + Parameters.ICON_URL_POST, imageView);
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
-        Date date = new Date((long)root.list.get(0).dt*1000);
-        SimpleDateFormat dateDayOfWeek = new SimpleDateFormat("E");
-        SimpleDateFormat dateDay = new SimpleDateFormat("EEE, d MMM yyyy HH:mm");
-        textViewDayOfWeek.setText(dateDayOfWeek.format(date));
-        textViewDay.setText(dateDay.format(date));
     }
+
+    public void lanzarPrevision(View view){
+        Intent intent = new Intent(this, PrevisionActivity.class);
+        intent.putExtra("lat",selectedCiudad.getLat());
+        intent.putExtra("lon",selectedCiudad.getLon());
+        intent.putExtra("nombreCiudad",selectedCiudad.getNombre());
+        startActivity(intent);
+    }
+
+
+
+
 }
